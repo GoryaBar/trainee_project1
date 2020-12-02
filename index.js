b4acf71f31d144dfda279db22801392fb9d77a15
Ihor Bardak <ibardak@protonmail.com>
2020-12-02 16:30:19 +0200

    testcommit1

---
+++ index.js
@@ -0,0 +1,9 @@
+import 'react-native-gesture-handler';
+import { AppRegistry } from 'react-native';
+import App from './src//App';
+import { name as appName } from './app.json';
+import { configure } from "mobx"
+
+configure({ useProxies: "never" }) // Or "ifavailable".
+
+AppRegistry.registerComponent(appName, () => App);
